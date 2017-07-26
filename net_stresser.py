511b4000906a110942fe57899c543646ede79455
Daniel Seymour <dannyseeless@gmail.com>
2017-07-26 00:26:00 +0000

    Added bandwidth option

--- net_stresser.py
+++ net_stresser.py
@@ -4,6 +4,7 @@ import multiprocessing as mp
 import argparse
 import subprocess
 import time
+import os
 from random import randint
 
 parser = argparse.ArgumentParser(description="Stress the VM network stack")
@@ -12,16 +13,19 @@ parser.add_argument('--host', dest="host")
 parser.add_argument('--duration', type=int, dest="duration", default="20")
 parser.add_argument('--ports', type=int, dest="ports", default="20")
 parser.add_argument('--server', dest="server", action="store_true")
+parser.add_argument('--bandwidth', type=int, dest="bandwidth", default="62500")
 
 args = parser.parse_args()
 
-def run_iperf_client(host, duration, port):
-    iperf_cmd = '/usr/bin/trickle -u 62500 -d 62500 iperf -c {0} -t {1} -p {2} --reportstyle=c'.format(host, duration, port)
-    subprocess.check_call(iperf_cmd.split())
+DEVNULL = open(os.devnull, 'w')
+
+def run_iperf_client(bandwidth, host, duration, port):
+    iperf_cmd = '/usr/bin/trickle -u {0} -d {0} iperf -c {1} -t {2} -p {3} --reportstyle=c'.format(bandwidth, host, duration, port)
+    subprocess.check_call(iperf_cmd.split(), stdout=DEVNULL, stderr=DEVNULL)
     
 def run_iperf_server(port):
     iperf_cmd = '/usr/bin/iperf -s -p {0} --reportstyle=c'.format(port)
-    subprocess.check_call(iperf_cmd.split())
+    subprocess.check_call(iperf_cmd.split(), stdout=DEVNULL, stderr=DEVNULL)
     
 def run_ping(host, count):
     ping_cmd = '/bin/ping -c {0} {1}'.format(count, host)
@@ -35,7 +39,7 @@ def main():
     processes = []
     if args.server is False:
         for i in range(args.ports):
-            p = mp.Process(target=run_iperf_client, args=(args.host, randint(1, 9), 5001 + i))
+            p = mp.Process(target=run_iperf_client, args=(args.bandwidth, args.host, randint(1, 9), 5001 + i))
             p.start()
             processes.append(p)
             
@@ -52,7 +56,7 @@ def main():
                 elif timer % 5 == 0:
                     del processes[i]
                     length = randint(1, 9)
-                    p = mp.Process(target=run_iperf_client, args=(args.host, length, 5001 + i))
+                    p = mp.Process(target=run_iperf_client, args=(args.bandwidth, args.host, length, 5001 + i))
                     p.start()
                     processes.insert(i, p)
                     started_processes += 1
